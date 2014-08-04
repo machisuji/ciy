03179bf87a350e480efab3520f317c9ae80060a7
Markus Kahl <machisuji@googlemail.com>
2014-08-04 17:06:08 +0200

    yaml parsing + proper scala console through jline

---
+++ src/main/scala/ciy/yaml.scala
@@ -0,0 +1,56 @@
+package ciy
+
+import scala.collection.JavaConverters._
+import org.yaml.snakeyaml._
+
+case class YamlMap(data: Map[String, Any]) {
+  def string(key: String): Option[String] = tryGet(key, classOf[String])
+  def int(key: String): Option[Int] = tryGet(key, classOf[java.lang.Integer]).map(_.toInt)
+  def long(key: String): Option[Long] = tryGet(key, classOf[java.lang.Long]).map(_.toLong)
+  def boolean(key: String): Option[Boolean] =
+    tryGet(key, classOf[java.lang.Boolean]).map(_.asInstanceOf[scala.Boolean])
+
+  def map(key: String): Option[YamlMap] =
+    tryGet(key, classOf[java.util.LinkedHashMap[String, Any]]).map(m => YamlMap(Map() ++ m.asScala))
+
+  def contains(key: String): Boolean = data.contains(key)
+
+  /**
+   * Tries to get the value for the given key.
+   *
+   * @return Returns the value of the given key given it has the assumed type.
+   */
+  private def tryGet[T](key: String, klass: Class[T]): Option[T] = {
+    if (key.contains(".")) tryGetAtPath(key, klass)
+    else data.get(key).flatMap { value =>
+      if (value.getClass == klass)
+        Some(value.asInstanceOf[T])
+      else
+        None
+    }
+  }
+
+  private def tryGetAtPath[T](keys: Seq[String], klass: Class[T]): Option[T] = {
+    val lastMap: Option[YamlMap] = keys.init.foldLeft(Some(this): Option[YamlMap]) {
+      case (mapOpt, key) => mapOpt.flatMap(_.map(key))
+    }
+
+    lastMap.flatMap(_.tryGet(keys.last, klass))
+  }
+
+  private def tryGetAtPath[T](keys: String, klass: Class[T]): Option[T] =
+    tryGetAtPath(keys.split("\\."), klass)
+
+  override def toString = data.toString.replace("Map", "YamlMap")
+}
+
+object YAML {
+  def fromFile(file: String): YamlMap = fromString(io.Source.fromFile(file).mkString)
+
+  def fromString(str: String): YamlMap = {
+    val yaml = new Yaml()
+    val data = yaml.load(str).asInstanceOf[java.util.Map[String, Any]].asScala
+
+    YamlMap(Map() ++ data)
+  }
+}
