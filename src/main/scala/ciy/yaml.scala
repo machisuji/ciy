d49bbf11a248edf4e3ab85d6be8a0ffdec13f92e
Markus Kahl <machisuji@googlemail.com>
2014-08-09 13:10:30 +0200

    yaml map refactoring using type classes

--- src/main/scala/ciy/yaml.scala
+++ src/main/scala/ciy/yaml.scala
@@ -3,54 +3,33 @@ package ciy
 import scala.collection.JavaConverters._
 import org.yaml.snakeyaml._
 
+/**
+ * Breaks if you try to parse maps with anything else than strings as keys for now.
+ */
 case class YamlMap(data: Map[String, Any]) {
-  def string(key: String): Option[String] = tryGet(key, classOf[String])
-  def int(key: String): Option[Int] = tryGet(key, classOf[java.lang.Integer]).map(_.toInt)
-  def long(key: String): Option[Long] = tryGet(key, classOf[java.lang.Long]).map(_.toLong)
-  def boolean(key: String): Option[Boolean] =
-    tryGet(key, classOf[java.lang.Boolean]).map(_.asInstanceOf[scala.Boolean])
+  def string(key: String): Option[String] = get[String](key)
+  def int(key: String): Option[Int] = get[Int](key)
+  def long(key: String): Option[Long] = get[Long](key)
+  def boolean(key: String): Option[Boolean] = get[Boolean](key)
 
-  def map(key: String): Option[YamlMap] =
-    tryGet(key, classOf[java.util.LinkedHashMap[String, Any]]).map(m => YamlMap(Map() ++ m.asScala))
+  def map(key: String): Option[YamlMap] = get[YamlMap](key)
+  def list[T](key: String)(implicit c: Converter[Any, T]): Option[List[T]] = get[List[T]](key)
 
-  def list[T](key: String)(implicit converter: Converter[Any, T]): Option[List[T]] = {
-    tryGet(key, classOf[java.util.ArrayList[Any]]).flatMap { jlist =>
-      val list: List[Any] = jlist.asScala.toList
-      val result: List[T] = list.flatMap(i => converter(i).toList)
-
-      if (result.isEmpty) None
-      else Some(result)
+  def get[T](key: String)(implicit c: Converter[Any, T]): Option[T] =
+    if (key.contains(".")) {
+      val keys = key.split("\\.")
+      mapOfPath(keys).flatMap(_.get[T](keys.last))
+    } else {
+      data.get(key).flatMap(c(_))
     }
-  }
 
   def contains(key: String): Boolean = data.contains(key)
   def keys: Set[String] = data.keys.toSet
 
-  /**
-   * Tries to get the value for the given key.
-   *
-   * @return Returns the value of the given key given it has the assumed type.
-   */
-  private def tryGet[T](key: String, klass: Class[T]): Option[T] = {
-    if (key.contains(".")) tryGetAtPath(key, klass)
-    else data.get(key).flatMap { value =>
-      if (value.getClass == klass)
-        Some(value.asInstanceOf[T])
-      else
-        None
-    }
-  }
-
-  private def tryGetAtPath[T](keys: Seq[String], klass: Class[T]): Option[T] = {
-    val lastMap: Option[YamlMap] = keys.init.foldLeft(Some(this): Option[YamlMap]) {
+  private def mapOfPath(keys: Seq[String]): Option[YamlMap] =
+    keys.init.foldLeft(Some(this): Option[YamlMap]) {
       case (mapOpt, key) => mapOpt.flatMap(_.map(key))
     }
-
-    lastMap.flatMap(_.tryGet(keys.last, klass))
-  }
-
-  private def tryGetAtPath[T](keys: String, klass: Class[T]): Option[T] =
-    tryGetAtPath(keys.split("\\."), klass)
 
   override def toString = data.toString.replace("Map", "YamlMap")
 }
@@ -85,6 +64,10 @@ trait Converter[F, T] {
 object Converter {
   implicit def string = new StringConverter
   implicit def int = new IntConverter
+  implicit def long = new LongConverter
+  implicit def boolean = new BooleanConverter
+  implicit def list[T](implicit ec: Converter[Any, T]) = new ListConverter[T]
+  implicit def map = new MapConverter
 }
 
 class StringConverter extends Converter[Any, String] {
@@ -99,4 +82,44 @@ class IntConverter extends Converter[Any, Int] {
     case i: java.lang.Integer => Some(i.toInt)
     case _ => None
   }
+}
+
+class LongConverter extends Converter[Any, Long] {
+  def apply(value: Any): Option[Long] = value match {
+    case l: java.lang.Long => Some(l.toLong)
+    case _ => None
+  }
+}
+
+class BooleanConverter extends Converter[Any, Boolean] {
+  def apply(value: Any): Option[Boolean] = value match {
+    case b: java.lang.Boolean => Some(b.asInstanceOf[scala.Boolean])
+    case _ => None
+  }
+}
+
+class ListConverter[T](implicit ec: Converter[Any, T]) extends Converter[Any, List[T]] {
+  def apply(value: Any): Option[List[T]] = value match {
+    case l: java.util.List[_] =>
+      if (l.size == 0) {
+        Some(List[T]())
+      } else if (l.size >= 1) {
+        val list = l.asScala.toList.flatMap(e => ec(e).toList)
+
+        if (list.isEmpty) None // failed to convert anything
+        else Some(list)
+      } else {
+        None
+      }
+    case _ => None
+  }
+}
+
+class MapConverter extends Converter[Any, YamlMap] {
+  def apply(value: Any): Option[YamlMap] =
+    if (value.isInstanceOf[java.util.Map[_, _]]) {
+      Some(YamlMap(Map() ++ value.asInstanceOf[java.util.Map[String, Any]].asScala))
+    } else {
+      None
+    }
 }
